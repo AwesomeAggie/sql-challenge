ce8010c7f8f6806c398663622b82a7b1d76273da
AwesomeAggie <woodulscollege@yahoo.com>
2023-03-02 22:47:15 -0600

    add partial file

---
+++ EmployedSQL/Data_Engineering.sql
@@ -0,0 +1,44 @@
+CREATE TABLE departments (
+    dept_no VARCHAR,
+    dept_name VARCHAR,
+    PRIMARY KEY (dept_no)
+	);
+
+CREATE TABLE titles (
+    title_id VARCHAR,
+    title VARCHAR,
+    PRIMARY KEY (title_id)
+	);
+
+CREATE TABLE employees (
+    emp_no INTEGER,
+    emp_title_id VARCHAR,
+    birth_date DATE,
+    first_name VARCHAR,
+    last_name VARCHAR,
+    sex VARCHAR,
+    hire_date DATE,
+    PRIMARY KEY (emp_no),
+    FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+	);
+
+CREATE TABLE dept_emp (
+    emp_no INTEGER,
+    dept_no VARCHAR,
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+	);
+
+CREATE TABLE dept_manager (
+	dept_no VARCHAR,
+    emp_no INTEGER,
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+	);
+	
+CREATE TABLE salaries (
+    emp_no INTEGER,
+    salary INTEGER,
+	PRIMARY KEY (emp_no),
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+	);
+
+
